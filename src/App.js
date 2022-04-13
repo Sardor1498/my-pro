c913dcbd8bd596181a22545250f030d762daa7d0
Sardor1498 <tsardor1498@gmail.com>
2022-04-13 19:11:14 +0500

    Sardor1498

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Salom gruppa</h1>
+     <h1 className="text-3xl font-bold text-red-300">Salom gruppa</h1>
+     <Tooltip title="search">
+      <Button shape="circle" icon={<SearchOutlined />} />
+    </Tooltip>
     </div>
   );
 }
