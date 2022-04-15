d2f3b2a7f3ff6f6818e5a25b6a634388c4f70421
Sardor1498 <tsardor1498@gmail.com>
2022-04-15 20:03:30 +0500

    Sardor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import './App.css';
-import { Button, Tooltip } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import React from 'react'
+import Layout from './components/Layout/Layout'
 
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold text-red-300">Salom gruppa</h1>
-     <Tooltip title="search">
-      <Button shape="circle" icon={<SearchOutlined />} />
-    </Tooltip>
+     <Layout />
     </div>
   );
 }
