1005c2518f3d6052066d1d96c99f9784a40ecb9a
abdurahmoniy <96695070+abdurahmoniy@users.noreply.github.com>
2022-04-18 18:31:57 +0500

    abdurahmoniy

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { SearchOutlined } from '@ant-design/icons';
 function App() {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold text-red-300">Salom gruppa</h1>
-     <Tooltip title="search">
-      <Button shape="circle" icon={<SearchOutlined />} />
-    </Tooltip>
+      <h1 className="text-3xl font-bold text-red-300">Salom gruppa</h1>
+      <Tooltip title="search">
+        <Button shape="circle" icon={<SearchOutlined />} />
+      </Tooltip>
     </div>
   );
 }
